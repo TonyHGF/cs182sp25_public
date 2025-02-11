22c470718e6a13d2dffbed2c439395980401e060
Naman Jain <naman1205jain@gmail.com>
2025-02-11 13:24:05 -0800

    upd: dis 1 plot_relu fix

--- dis00/q2/code/helpers.py
+++ dis00/q2/code/helpers.py
@@ -17,16 +17,17 @@ def plot_data(X, y, X_test, y_test):
     clip_bound = 2.5
     plt.xlim(0, 1)
     plt.ylim(-clip_bound, clip_bound)
-    plt.scatter(X[:, 0], y, c='darkorange', s=40.0, label='training data points')
-    plt.plot(X_test, y_test, '--', color='royalblue', linewidth=2.0, label='Ground truth')
+    plt.scatter(X[:, 0], y, c="darkorange", s=40.0, label="training data points")
+    plt.plot(
+        X_test, y_test, "--", color="royalblue", linewidth=2.0, label="Ground truth"
+    )
 
 
 def plot_relu(bias, slope):
-    plt.scatter([-bias / slope], 0, c='darkgrey', s=40.0)
-    if slope > 0 and bias < 0:
-        plt.plot([0, -bias / slope, 1], [0, 0, slope * (1 - bias)], ':')
-    elif slope < 0 and bias > 0:
-        plt.plot([0, -bias / slope, 1], [-bias * slope, 0, 0], ':')
+    forward_fn = lambda x: max(0, slope * x + bias)
+    x_break = -bias / slope
+    plt.scatter([x_break], 0, c="darkgrey", s=40.0)
+    plt.plot([0, x_break, 1], [forward_fn(0), 0, forward_fn(1)], ":")
 
 
 def plot_relus(params):
@@ -38,7 +39,7 @@ def plot_relus(params):
 
 def plot_function(X_test, net):
     y_pred = net(to_torch(X_test))
-    plt.plot(X_test, to_numpy(y_pred), '-', color='forestgreen', label='prediction')
+    plt.plot(X_test, to_numpy(y_pred), "-", color="forestgreen", label="prediction")
 
 
 def plot_update(X, y, X_test, y_test, net, state=None):
@@ -49,10 +50,21 @@ def plot_update(X, y, X_test, y_test, net, state=None):
     plot_function(X_test, net)
     plot_data(X, y, X_test, y_test)
     plt.legend()
-    plt.show();
+    plt.show()
 
 
-def train_network(X, y, X_test, y_test, net, optim, n_steps, save_every, initial_weights=None, verbose=False):
+def train_network(
+    X,
+    y,
+    X_test,
+    y_test,
+    net,
+    optim,
+    n_steps,
+    save_every,
+    initial_weights=None,
+    verbose=False,
+):
     loss = torch.nn.MSELoss()
     y_torch = to_torch(y.reshape(-1, 1))
     X_torch = to_torch(X)
@@ -66,13 +78,13 @@ def train_network(X, y, X_test, y_test, net, optim, n_steps, save_every, initial
         step_loss.backward()
         optim.step()
         if (s + 1) % save_every == 0 or s == 0:
-#             plot_update(X, y, X_test, y_test, net)
+            #             plot_update(X, y, X_test, y_test, net)
             history[s + 1] = {}
-            history[s + 1]['state'] = copy.deepcopy(net.state_dict())
+            history[s + 1]["state"] = copy.deepcopy(net.state_dict())
             with torch.no_grad():
                 test_loss = loss(to_torch(y_test.reshape(-1, 1)), net(to_torch(X_test)))
-            history[s + 1]['train_error'] = to_numpy(step_loss).item()
-            history[s + 1]['test_error'] = to_numpy(test_loss).item()
+            history[s + 1]["train_error"] = to_numpy(step_loss).item()
+            history[s + 1]["test_error"] = to_numpy(test_loss).item()
             if verbose:
                 print("SGD Iteration %d" % (s + 1))
                 print("\tTrain Loss: %.3f" % to_numpy(step_loss).item())
@@ -87,41 +99,40 @@ def train_network(X, y, X_test, y_test, net, optim, n_steps, save_every, initial
 
 def plot_test_train_errors(history):
     sample_points = np.array(list(history.keys()))
-    etrain = [history[s]['train_error'] for s in history]
-    etest = [history[s]['test_error'] for s in history]
-    plt.plot(sample_points / 1e3, etrain, label='Train Error')
-    plt.plot(sample_points / 1e3, etest, label='Test Error')
+    etrain = [history[s]["train_error"] for s in history]
+    etest = [history[s]["test_error"] for s in history]
+    plt.plot(sample_points / 1e3, etrain, label="Train Error")
+    plt.plot(sample_points / 1e3, etest, label="Test Error")
     plt.xlabel("Iterations (1000's)")
     plt.ylabel("MSE")
-    plt.yscale('log')
+    plt.yscale("log")
     plt.legend()
-    plt.show();
+    plt.show()
 
 
 def make_iter_slider(iters):
     # print(iters)
     return widgets.SelectionSlider(
-        options=iters,
-        value=1,
-        description='SGD Iterations: ',
-        disabled=False
+        options=iters, value=1, description="SGD Iterations: ", disabled=False
     )
 
 
 def history_interactive(history, idx, X, y, X_test, y_test, net):
-    plot_update(X, y, X_test, y_test, net, state=history[idx]['state'])
+    plot_update(X, y, X_test, y_test, net, state=history[idx]["state"])
     plt.show()
-    print("Train Error: %.3f" % history[idx]['train_error'])
-    print("Test Error: %.3f" % history[idx]['test_error'])
+    print("Train Error: %.3f" % history[idx]["train_error"])
+    print("Test Error: %.3f" % history[idx]["test_error"])
 
 
 def make_history_interactive(history, X, y, X_test, y_test, net):
     sample_points = list(history.keys())
-    return interactive(history_interactive,
-                       history=fixed(history),
-                       idx=make_iter_slider(sample_points),
-                       X=fixed(X),
-                       y=fixed(y),
-                       X_test=fixed(X_test),
-                       y_test=fixed(y_test),
-                       net=fixed(net))
+    return interactive(
+        history_interactive,
+        history=fixed(history),
+        idx=make_iter_slider(sample_points),
+        X=fixed(X),
+        y=fixed(y),
+        X_test=fixed(X_test),
+        y_test=fixed(y_test),
+        net=fixed(net),
+    )
